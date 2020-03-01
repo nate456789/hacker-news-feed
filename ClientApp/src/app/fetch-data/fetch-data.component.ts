8255cae0cf84040cc0d3abc71424b54809124934
Nate Battinich <nate456789@gmail.com>
2020-03-01 14:17:03 -0500

    Added refresh timer and icon

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, Inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
@@ -7,13 +7,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public feedStories: FeedStories[];
+  private interval;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+
+    this.refreshData(http, baseUrl);
+    this.interval = setInterval(() => {
+      this.refreshData(http, baseUrl);
+    }, 60000); // Refresh the feed every minute
+
+  }
+
+  refreshData(http: HttpClient, baseUrl: string) {
+    console.log('Data Refreshed');
     http.get<FeedStories[]>(baseUrl + 'NewsFeed').subscribe(result => {
       this.feedStories = result;
     }, error => console.error(error));
+
   }
 }
+
 
 interface FeedStories {
   id: number;
